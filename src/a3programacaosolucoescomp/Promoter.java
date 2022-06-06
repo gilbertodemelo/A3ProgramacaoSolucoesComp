f40081eca13245e9f1fa523db9707dc558a4d87e
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2022-06-06 19:32:30 -0300

    primeiro commit

---
+++ src/a3programacaosolucoescomp/Promoter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a3programacaosolucoescomp;
+
+/**
+ *
+ * @author gilbertodemelojr
+ */
+public class Promoter implements Usuario {
+    
+    /**
+     * Constructor
+     */
+    public Promoter(String primeiroNome, String sobreNome, String numTel) {
+        super(primeiroNone, sobreNoome, numTel)
+    }
+    
+}
---
+++ src/a3programacaosolucoescomp/Usuario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package a3programacaosolucoescomp;
+
+public interface Usuario {
+    
+    public String setPrimeiroNome();
+    public String setSobrenome();
+    public String setEmail();
+    public String setLogradouro();
+    public String setTelefone();
+    public String setRg();
+    public String setCpf();
+  
+}
