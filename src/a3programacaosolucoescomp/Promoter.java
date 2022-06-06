14b236af2964fcb65babc0abc642a6f2d0e23a21
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2022-06-06 19:57:59 -0300

    finalizada a criacao da classe Promoter

--- src/a3programacaosolucoescomp/Promoter.java
+++ src/a3programacaosolucoescomp/Promoter.java
@@ -4,10 +4,6 @@
  */
 package a3programacaosolucoescomp;
 
-/**
- *
- * @author gilbertodemelojr
- */
 public class Promoter implements Usuario {
     
     // Variáveis de instância
@@ -22,6 +18,15 @@ public class Promoter implements Usuario {
   
     /**
      * Constructor
+     * @param primeiroNome
+     * @param sobreNome
+     * @param email
+     * @param numTel
+     * @param rg
+     * @param cpf
+     * @param empresa
+     * @param cnpj
+     * 
      */
     public Promoter(String primeiroNome, String sobreNome, String email, 
             String numTel, String rg, String cpf, String empresa, String cnpj) {
@@ -37,7 +42,37 @@ public class Promoter implements Usuario {
        
     }
     
-    //Métdoso Acessores
-    public void setPrimeiroNome(String primeiroNome) { this.primeiroNome = primeiroNome; } 
-  
+    // Métodos Mutadores
+    @Override
+    public void setPrimeiroNome(String primeiroNome) { this.primeiroNome = primeiroNome; }
+    @Override
+    public void setSobrenome(String sobreNome) { this.sobreNome = sobreNome; }
+    @Override
+    public void setEmail(String email) { this.email = email; }
+    @Override
+    public void setTelefone(String numTel) { this.numeroTelefone = numTel; }
+    @Override
+    public void setRg(String rg) { this.rg = rg;}
+    @Override
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public void setEmpresa(String empresa) { this.empresa = empresa; }
+    public void setCnpjEmpresa(String cnpj) { this.cnpjEmpresa = cnpj; }
+    
+    // Métodos Acessores 
+    @Override
+    public String getPrimeiroNome() { return this.primeiroNome; }
+    @Override
+    public String getSobrenome() { return this.sobreNome; }
+    @Override
+    public String getEmail() { return this.email; }
+    @Override
+    public String getNumTelefone() { return this.numeroTelefone; }
+    @Override
+    public String getRg() { return this.rg; }
+    @Override
+    public String getCpf() { return this.cpf; }
+    public String getEmpresa() { return this.empresa; }
+    public String getCnpjEmpresa() { return this.cnpjEmpresa; }
+
+   
 }
--- src/a3programacaosolucoescomp/Usuario.java
+++ src/a3programacaosolucoescomp/Usuario.java
@@ -6,11 +6,20 @@ package a3programacaosolucoescomp;
 
 public interface Usuario {
     
-    public void setPrimeiroNome();
-    public void setSobrenome();
-    public void setEmail();
-    public void setTelefone();
-    public void setRg();
-    public void setCpf();
+    // Mutadores);
+    public void setPrimeiroNome(String primeiroNome);
+    public void setSobrenome(String sobreNome);
+    public void setEmail(String email);
+    public void setTelefone(String numTel);
+    public void setRg(String rg);
+    public void setCpf(String cpf);
+    
+    // Acessores
+    public String getPrimeiroNome();
+    public String getSobrenome();
+    public String getEmail();
+    public String getNumTelefone();
+    public String getRg();
+    public String getCpf();
   
 }
