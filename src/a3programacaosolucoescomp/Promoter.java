9b2cd3ebe8014acacc4285043b91151835f21921
HenriBDev <henribdev@gmail.com>
2022-06-07 12:08:44 -0300

    :bug: fix - Consertando commit anterior

--- src/a3programacaosolucoescomp/Promoter.java
+++ src/a3programacaosolucoescomp/Promoter.java
@@ -8,100 +8,25 @@ import java.util.ArrayList;
 import java.util.Date;
 
 
-public class Promoter implements Usuario {
+public class Promoter extends Usuario {
     
     // Variáveis de instância
-    private String primeiroNome;
-    private String sobreNome;
-    private String email;
-    private String numeroTelefone;
-    private String rg;
-    private String cpf;
     private String empresa;
     private String cnpjEmpresa;
-    private ArrayList<Evento> eventos; // ArrayList contendo lista de eventos do promoter
-  
-    /**
-     * Constructor
-     * @param primeiroNome
-     * @param sobreNome
-     * @param email
-     * @param numTel
-     * @param rg
-     * @param cpf
-     * @param empresa
-     * @param cnpj
-     * O Constructor instancia o objeto com uma ArrayList de Eventos fazia
-     */
-    public Promoter(String primeiroNome, String sobreNome, String email, 
-            String numTel, String rg, String cpf, String empresa, String cnpj) {
-       this.primeiroNome = primeiroNome;
-       this.sobreNome = sobreNome;
-       this.email = email;
-       this.numeroTelefone = numTel;
-       this.rg = rg;
-       this.cpf = cpf;
-       this.empresa = empresa;
-       this.cnpjEmpresa = cnpj;
-       this.eventos = new ArrayList<>();
-              
+    
+    // Método ocnstrutor
+    public Promoter(String nome, String sobrenome, String email, String senha, String cpf, String rg, String telefone, String cep, String endereco, String contato, String tipoUsuario, String empresa, String cnpj){ 
+        super(nome, sobrenome, email, senha, cpf, rg, telefone, cep, endereco, contato, tipoUsuario);
+        this.empresa = empresa;
+        this.cnpjEmpresa = cnpj;
     }
     
     // Métodos Mutadores
-    @Override
-    public void setPrimeiroNome(String primeiroNome) { this.primeiroNome = primeiroNome; }
-    @Override
-    public void setSobrenome(String sobreNome) { this.sobreNome = sobreNome; }
-    @Override
-    public void setEmail(String email) { this.email = email; }
-    @Override
-    public void setTelefone(String numTel) { this.numeroTelefone = numTel; }
-    @Override
-    public void setRg(String rg) { this.rg = rg;}
-    @Override
-    public void setCpf(String cpf) { this.cpf = cpf; }
     public void setEmpresa(String empresa) { this.empresa = empresa; }
     public void setCnpjEmpresa(String cnpj) { this.cnpjEmpresa = cnpj; }
   
     
     // Métodos Acessores 
-    @Override
-    public String getPrimeiroNome() { return this.primeiroNome; }
-    @Override
-    public String getSobrenome() { return this.sobreNome; }
-    @Override
-    public String getEmail() { return this.email; }
-    @Override
-    public String getNumTelefone() { return this.numeroTelefone; }
-    @Override
-    public String getRg() { return this.rg; }
-    @Override
-    public String getCpf() { return this.cpf; }
     public String getEmpresa() { return this.empresa; }
     public String getCnpjEmpresa() { return this.cnpjEmpresa; }
-    
-      /**
-     * criaEvento - cria um objeto Evento e salva na
-     * ArrayList de eventos do objeto Promoter
-     * @param nomeEvento
-     * @param data
-     * @param local
-     * @param promoter 
-     */
-    public void criaEvento(String nomeEvento, Date data, String local, Promoter promoter) {
-        eventos.add(new Evento(nomeEvento, data, local, promoter));
-    }
-    
-    /**
-     * procuraEventos - mostra os eventos de um Promoter
-     */
-    public void procuraEventos() {
-        if (this.eventos.isEmpty()) {
-            System.out.println("Não há eventos agendados.");
-        } else {
-            eventos.forEach((item) -> System.out.println(item.toString()));
-        }
-    }
-
-   
 }
--- src/a3programacaosolucoescomp/Usuario.java
+++ src/a3programacaosolucoescomp/Usuario.java
@@ -4,22 +4,55 @@
  */
 package a3programacaosolucoescomp;
 
-public interface Usuario {
+public class Usuario {
+    
+    public static int contadorId = 1;
+    protected int id = contadorId;
+    protected String cpf, rg, email, senha, telefone, endereco, cep, primeiroNome,
+            sobrenome, tipoUsuario, contato;
+    
+    // Método construtor
+    public Usuario(){}
+    public Usuario(String nome, String sobrenome, String email, String senha, String cpf, String rg, String telefone, String cep, String endereco, String contato, String tipoUsuario){
+        this.primeiroNome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.telefone = telefone;
+        this.cep = cep;
+        this.endereco = endereco;
+        this.contato = contato;
+        this.tipoUsuario = tipoUsuario;
+        contadorId++;
+        this.id = contadorId;
+    }
     
     // Mutadores);
-    public void setPrimeiroNome(String primeiroNome);
-    public void setSobrenome(String sobreNome);
-    public void setEmail(String email);
-    public void setTelefone(String numTel);
-    public void setRg(String rg);
-    public void setCpf(String cpf);
+    public void setCpf(String cpf){ this.cpf = cpf; };
+    public void setRg(String rg){ this.rg = rg; };
+    public void setEmail(String email){ this.email = email; };
+    public void setSenha(String senha){ this.senha = senha; };
+    public void setTelefone(String telefone){ this.telefone = telefone; };
+    public void setEndereco(String endereco){ this.endereco = endereco; };
+    public void setCep(String cep){ this.cep = cep; };
+    public void setPrimeiroNome(String primeiroNome){ this.primeiroNome = primeiroNome; };
+    public void setSobrenome(String sobrenome){ this.sobrenome = sobrenome; };
+    public void setTipoUsuario(String tipoUsuario){ this.tipoUsuario = tipoUsuario; };
+    public void setContato(String contato){ this.contato = contato; };
     
     // Acessores
-    public String getPrimeiroNome();
-    public String getSobrenome();
-    public String getEmail();
-    public String getNumTelefone();
-    public String getRg();
-    public String getCpf();
-  
+    public int getId(){ return this.id; };
+    public String getCpf(){ return this.cpf; };
+    public String getRg(){ return this.rg; };
+    public String getEmail(){ return this.email; };
+    public String getSenha(){ return this.senha; };
+    public String getTelefone(){ return this.telefone; };
+    public String getEndereco(){ return this.endereco; };
+    public String getCep(){ return this.cep; };
+    public String getPrimeiroNome(){ return this.primeiroNome; };
+    public String getSobrenome(){ return this.sobrenome; };
+    public String getTipoUsuario(){ return this.tipoUsuario; };
+    public String getContato(){ return this.contato; };
 }
